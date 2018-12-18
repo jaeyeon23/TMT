265753a3208c2f69e91e78986b0a6fa52d65319c
WooJeongYoon <wjddbs0506@naver.com>
2018-12-18 15:07:29 +0900

    2018-12-18

--- src/basket/BasketVO.java
+++ src/basket/BasketVO.java
@@ -1,5 +1,162 @@
 package basket;
 
 public class BasketVO {
-
+	
+	private int seq_no;
+	private String name;
+	private int price;
+	private String content;
+	private String country;
+	private String region;
+	private String image1;
+	private String image2;
+	private String image3;
+	private String id;
+	private String day;
+	private String checkin;
+	private String checkout;
+	private String ad;
+	private String dd;
+	private String hai;
+	private int no;
+	private String seat;
+	private String room;
+	private String people;
+	private String air;
+	private int amount;
+	
+	public int getSeq_no() {
+		return seq_no;
+	}
+	public void setSeq_no(int seq_no) {
+		this.seq_no = seq_no;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public String getCountry() {
+		return country;
+	}
+	public void setCountry(String country) {
+		this.country = country;
+	}
+	public String getRegion() {
+		return region;
+	}
+	public void setRegion(String region) {
+		this.region = region;
+	}
+	public String getImage1() {
+		return image1;
+	}
+	public void setImage1(String image1) {
+		this.image1 = image1;
+	}
+	public String getImage2() {
+		return image2;
+	}
+	public void setImage2(String image2) {
+		this.image2 = image2;
+	}
+	public String getImage3() {
+		return image3;
+	}
+	public void setImage3(String image3) {
+		this.image3 = image3;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getDay() {
+		return day;
+	}
+	public void setDay(String day) {
+		this.day = day;
+	}
+	public String getCheckin() {
+		return checkin;
+	}
+	public void setCheckin(String checkin) {
+		this.checkin = checkin;
+	}
+	public String getCheckout() {
+		return checkout;
+	}
+	public void setCheckout(String checkout) {
+		this.checkout = checkout;
+	}
+	public String getAd() {
+		return ad;
+	}
+	public void setAd(String ad) {
+		this.ad = ad;
+	}
+	public String getDd() {
+		return dd;
+	}
+	public void setDd(String dd) {
+		this.dd = dd;
+	}
+	public String getHai() {
+		return hai;
+	}
+	public void setHai(String hai) {
+		this.hai = hai;
+	}
+	public int getNo() {
+		return no;
+	}
+	public void setNo(int no) {
+		this.no = no;
+	}
+	public String getSeat() {
+		return seat;
+	}
+	public void setSeat(String seat) {
+		this.seat = seat;
+	}
+	public String getRoom() {
+		return room;
+	}
+	public void setRoom(String room) {
+		this.room = room;
+	}
+	public String getPeople() {
+		return people;
+	}
+	public void setPeople(String people) {
+		this.people = people;
+	}
+	public String getAir() {
+		return air;
+	}
+	public void setAir(String air) {
+		this.air = air;
+	}
+	public int getAmount() {
+		return amount;
+	}
+	public void setAmount(int amount) {
+		this.amount = amount;
+	}
+	
+	
 }
